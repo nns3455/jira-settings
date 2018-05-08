8ccec5de0720998dda17c0f6783103a4542bd876
Nicholas Swanson <nicholas_swanson@ultimatesoftware.com>
2018-05-08 17:17:59 -0400

    Added options page

---
+++ options/options.js
@@ -0,0 +1,48 @@
+$(function() {
+	restore_options();
+	$('#save').click(save_options);
+	
+	// Saves options to chrome.storage
+	function save_options() {
+		var options = {};
+		var $optionElements = $(".option");
+		
+		$optionElements.each(function() {
+			add_element_option(this, options);
+		});
+		
+		chrome.storage.sync.set({
+			jira_helper_options: options
+		}, function() {
+			console.log("Saved:");
+			console.log(options);
+		});
+	}
+
+	// Restores select box and checkbox state using the preferences
+	// stored in chrome.storage.
+	function restore_options() {
+		// Use default value color = 'red' and likesColor = true.
+		chrome.storage.sync.get({
+			jira_helper_options: {}
+		}, function(items) {
+			var options = items["jira_helper_options"];
+			
+			$.each(options, function(id, value) {
+				var $element = $(`[data-option-id='${id}']`);
+				if ($element.is(":checkbox")) {
+					$element.prop("checked", value);
+				}
+			});
+		});
+	}
+		
+	function add_element_option(element, options) {
+		var $element = $(element);
+		
+		if ($element.is(":checkbox")) {
+			var id = $element.data("option-id");
+			options[id] = $element.is(":checked");
+		}
+	}
+});
